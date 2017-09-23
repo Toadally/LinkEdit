ece398b894e32ef6ae5d8ee5d4041865b5419381
Drew Gifford <legos30000@hotmail.com>
2017-09-23 00:01:13 -0400

    no message

--- js/index.js
+++ js/index.js
@@ -1,16 +1,42 @@
 $(document).ready(function(){
-  var theme = "ace-cobalt";
   $("#sidebar").addClass(theme);
   $("#top-selector").addClass(theme);
   $("body").addClass(theme);
   $("#top-selector").css("border-bottom","1px solid "+$(".ace_gutter").css("color"));
-  $("#editor").css("border-left","1px solid "+$(".ace_gutter").css("color"));
-  $("#top-selector").css("color",$(".ace_gutter").css("color"));
-  $("#top-selector .active").css({
-    "color":"white",
-    "text-decoration":"underline"
-  });
+  $(".header").css("border-bottom","1px solid "+$(".ace_gutter").css("color"));
+  $("#sidebar").css("border-right","1px solid "+$(".ace_gutter").css("color"));
   var n = $("#top-selector div").length;
   var w = (100/n);
   $("#top-selector div").width(w+'%');
+  $(".ace_gutter").ready(function(){
+  $("#top-selector").css("color",$(".ace_gutter").css("color"));});
+
+
+  $("#top-selector div").hover(function(){
+
+    $(this).find("i").fadeIn(125);
+
+  }, function(){
+
+    $(this).find("i").fadeOut(125);
+
+  });
+
+  $(".tab").click(function(){
+
+    $(this).siblings().find("span").removeClass('active');
+    $(this).siblings().removeClass('activeTab');
+    $(this).find('span').addClass("active");
+    $(this).addClass('activeTab');
+
+  });
+
+  $("#top-selector i").click(function(){
+
+    $(this).parent().animate({
+      "width":"0px"
+    }, 75, function(){$(this).remove()});
+
+
+  });
 });
---
+++ js/load.js
@@ -0,0 +1,10 @@
+$('head').append("<link rel='stylesheet' href='./css/load.css'>");
+
+
+
+$("body").ready(function(){
+
+  $(".spinner").fadeOut(750);
+  $(".spin-back").fadeOut(750);
+
+});
